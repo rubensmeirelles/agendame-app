c4c2532633a629a3694ecb4cc0e04ce1a9ac1d99
Rubens <rubens.meirelles@postalsaude.com.br>
2025-08-15 17:12:16 -0300

    Pinia

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,67 +1,82 @@
-<script setup lang="ts">
-  import { ref } from 'vue';
-  import axios from 'axios';
+<template>
+  <v-alert v-if="feedbackMessage" class="mb-2" color="error">{{ feedbackMessage }}</v-alert>
+  <v-form @submit.prevent="submit">
+    <v-row class="d-flex mb-3">
+      <v-col cols="12 font-weight-bold mb-1"><v-label>E-mail</v-label>
+        <v-text-field v-model="email" color="primary" :error-messages="errors.email" variant="outlined" />
+      </v-col>
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">Senha</v-label>
+        <v-text-field v-model="password" color="primary" :error-messages="errors.password" type="password"
+          variant="outlined" />
+      </v-col>
+      <v-col class="pt-0" cols="12">
+        <div class="d-flex flex-wrap align-center">
+          <div class="ml-sm-auto">
+            <RouterLink class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium" to="/">
+              Esqueceu sua senha?</RouterLink>
+          </div>
+        </div>
+      </v-col>
+      <v-col class="pt-0" cols="12">
+        <v-btn block color="primary" flat size="large" type="submit" :loading="isSubmitting">Entrar</v-btn>
+      </v-col>
+    </v-row>
+    <div>
+      <h6 class="text-h6 text-muted font-weight-medium d-flex justify-center align-center mt-3">
+        Novo no AgendaMe?
+        <RouterLink class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium pl-2"
+          :to="{ name: 'register' }">
+          Crie sua conta</RouterLink>
+      </h6>
+    </div>
+  </v-form>
+</template>
 
-  const email = ref('');
-  const password = ref('');
+<script setup>
+import { ref } from 'vue';
+import axios from '@/plugins/axios';
+import { useForm, useField } from 'vee-validate';
+import { object, string } from 'yup';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
 
-  function login (){
-    axios.defaults.withCredentials = true;
-    axios.defaults.withXSRFToken = true;
-    axios.get('http://localhost:8000/sanctum/csrf-cookie')
-      .then(() => {
-        axios.post('http://localhost:8000/api/login', {
-          email: 'test@example.com',
-          password: 'password',
+const schema = object({
+  email: string().required().email().label('E-mail'),
+  password: string().required().label('Senha'),
+})
+
+const { handleSubmit, errors, isSubmitting } = useForm({
+  validationSchema: schema
+})
+
+const submit = handleSubmit(async (values) => {
+  await login(values)
+})
+
+const { value: email } = useField('email')
+const { value: password } = useField('password')
+
+const feedbackMessage = ref('');
+
+const authStore = useAuthStore()
+
+const router = useRouter();
+
+function login(values) {
+  feedbackMessage.value = '';
+
+  authStore
+    .sanctum()
+    .then(() => {
+      authStore
+        .login(values.email, values.password)
+        .then(() => {
+          router.push({ name: 'dashboard' })
+        }).catch(() => {
+          feedbackMessage.value = 'E-mail ou senha inválidos!'
         })
-      })
-  }
+    })
+}
 
 </script>
-
-<template>
-  <v-row class="d-flex mb-3">
-    <v-col cols="12">ont-weight-bold mb-1">E-mail</v-label>
-      <v-text-field v-model="email" color="primary" hide-details variant="outlined" />
-    </v-col>
-    <v-col cols="12">
-      <v-label class="font-weight-bold mb-1">Senha</v-label>
-      <v-text-field
-        v-model="password"
-        color="primary"
-        hide-details
-        type="password"
-        variant="outlined"
-      />
-    </v-col>
-    <v-col class="pt-0" cols="12">
-      <div class="d-flex flex-wrap align-center">
-        <div class="ml-sm-auto">
-          <RouterLink
-            class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium"
-            to="/"
-          >Esqueceu sua senha?</RouterLink>
-        </div>
-      </div>
-    </v-col>
-    <v-col class="pt-0" cols="12">
-      <v-btn
-        block
-        color="primary"
-        flat
-        size="large"
-        @click="login()"
-      >Entrar</v-btn>
-    </v-col>
-  </v-row>
-  <div>
-    <h6 class="text-h6 text-muted font-weight-medium d-flex justify-center align-center mt-3">
-      Novo no AgendaMe?
-      <RouterLink
-        class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium pl-2"
-        :to="{ name: 'register' }"
-      >
-        Crie sua conta</RouterLink>
-    </h6>
-  </div>
-</template>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import App from './App.vue'
 import router from './router'
 import { createApp } from 'vue'
 import vuetify from './plugins/vuetify'
+import pinia from './plugins/pinia'
 import '@/scss/style.scss'
+import './plugins/yup'
+import './plugins/axios'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+app.use(pinia)
 app.mount('#app')
